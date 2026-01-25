b9e7ab9403f05eadb08f93359ac59558371ba1e5
Darul Itqan <pp.darul.itqan@gmail.com>
2026-01-26 01:00:26 +0800

    versi-xray.sh

--- versi-xray.sh
+++ versi-xray.sh
@@ -27,27 +27,27 @@ BYlw='\e[48;5;226m' #yellow backgroud
 xrays_path=$(which xray)
 xrays_version=$("$xrays_path" --version 2>&1)
 current_version=$(echo "$xrays_version" | awk '/Xray/{print $2}')
-# / / Ambil Xray Core Version Terbaru
+# Ambil Xray Core Version Terbaru
 latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-# / / Installation Xray Core
+# Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/Xray-linux-64.zip"
 clear
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}" | lolcat
 echo -e "\e[48;5;226;1;38;5;88m            XRAY CORE VERSION           \e[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}" | lolcat
-${BIWhite}      Current version : ${HT}v${current_version} ${NC}
-${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC} | lolcat
+echo -e "${BIWhite}      Current version : ${HT}v${current_version} ${NC}
+${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}
    [\033[1;36m 1 \033[0m] Xray Core v1.5.4
    [\033[1;36m 2 \033[0m] Xray Core v1.6.1
    [\033[1;36m 3 \033[0m] Xray Core v1.7.2
    [\033[1;36m 4 \033[0m] Xray Core v1.7.5
    [\033[1;36m 5 \033[0m] Xray Core v1.8.4
    [\033[1;36m 6 \033[0m] Xray Core v${latest_version} ${HT}< latest${NC}
-${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC} | lolcat
+${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}
    [\033[1;36m 7 \033[0m] Xray Core Mod v1.6.5   
    [\033[1;36m 8 \033[0m] Xray Core Mod v1.7.2-1
    [\033[1;36m 9 \033[0m] Xray Core Mod v25.3.31
-${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC} | lolcat
+${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}
   \033[1;36m  10.\033[0m Check Xray Core Version
   \033[1;36m   0.\033[0m Back to main menu"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}" | lolcat
@@ -91,7 +91,7 @@ xraychanger
 6)
 clear
 mv $xrays_path $xrays_path.bakk && curl -L https://github.com/XTLS/Xray-core/releases/download/v$latest_version/Xray-linux-64.zip > Xray-linux-64.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
-#mv $xrays_path $xrays_path.bakk && curl -L https://github.com/XTLS/Xray-core/releases/download/v${latest_version}/xray-linux-64.zip > xray-linux-64.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
+mv $xrays_path $xrays_path.bakk && curl -L https://github.com/XTLS/Xray-core/releases/download/v${latest_version}/xray-linux-64.zip > xray-linux-64.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
 read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
 xraychanger
 ;;
@@ -110,7 +110,7 @@ xraychanger
 9)
 clear
 mv $xrays_path $xrays_path.bakk && curl -L https://github.com/NevermoreSSH/Xcore-custompath/releases/download/v25.3.31/Xray-linux-64-v25.3.31.zip > Xray.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
-#mv $xrays_path $xrays_path.bakk && curl -L https://github.com/XTLS/Xray-core/releases/download/v${latest_version}/xray-linux-64.zip > xray-linux-64.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
+mv $xrays_path $xrays_path.bakk && curl -L https://github.com/XTLS/Xray-core/releases/download/v${latest_version}/xray-linux-64.zip > xray-linux-64.zip && unzip *.zip && mv xray /usr/local/bin && chmod +x $xrays_path && rm *.zip *.dat LICENSE README.md && xray version
 read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
 xraychanger
 ;;
@@ -118,7 +118,7 @@ xraychanger
 clear
 xray version
 echo -e "[ ${green}INFO${NC} ] Back to menu in 5 sec . . . "
-##echo -e "\033[0;32mBack to menu in 5 sec\033[0;32m"
+echo -e "\033[0;32mBack to menu in 5 sec\033[0;32m"
 sleep 5
 xraychanger
 ;;
@@ -129,7 +129,7 @@ menu
 *)
 clear
 echo -e "[ ${red}INFO${NC} ] Please enter an correct number . . . "
-#echo -e "\e[1;31m Please enter an correct number\e[1;31m"
+echo -e "\e[1;31m Please enter an correct number\e[1;31m"
 sleep 3
 xraychanger
 ;;
